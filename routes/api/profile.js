52c96c0ab9b0eaf1a1684d54ffc3f09fb5e59f91
Michael Raynes <raynesmike@yahoo.com>
2021-09-13 20:45:41 -0700

    Get logged in user profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,9 +1,28 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth');
 
-// @route   Get api/profile
-// @desc    Test route
+const Profile = require('../../models/Profile');
+const User = require('../../models/User');
+
+// @route   Get api/profile/me
+// @desc    Get current user profile
 // @access  Public
-router.get('/', (req, res) => res.send('Profile Route'));
+// adding auth as 2nd parameter makes it secure with tokens
+router.get('/me', auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({ user: req.user.id }).populate(
+      'user',
+      ['name', 'avatar']
+    );
+
+    if (!profile) {
+      return res.status(400).json({ msg: 'There is no profile for this user' });
+    }
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
 
 module.exports = router;
