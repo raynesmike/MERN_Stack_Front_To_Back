7be3000390325e529337baf600be26df808c2422
Michael Raynes <raynesmike@yahoo.com>
2021-09-29 18:12:09 -0700

    loadUser, logout functionality that correspond if user is login or not with auth

--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -3,6 +3,9 @@ import {
   REGISTER_FAIL,
   USER_LOADED,
   AUTH_ERROR,
+  LOGIN_SUCCESS,
+  LOGIN_FAIL,
+  LOGOUT,
 } from '../actions/types';
 
 const initialState = {
@@ -12,7 +15,7 @@ const initialState = {
   user: null,
 };
 
-export default function foo(state = initialState, action) {
+export default function auth(state = initialState, action) {
   const { type, payload } = action;
 
   switch (type) {
@@ -24,6 +27,7 @@ export default function foo(state = initialState, action) {
         user: payload,
       };
     case REGISTER_SUCCESS:
+    case LOGIN_SUCCESS:
       localStorage.setItem('token', payload.token);
       return {
         ...state,
@@ -33,6 +37,8 @@ export default function foo(state = initialState, action) {
       };
     case REGISTER_FAIL:
     case AUTH_ERROR:
+    case LOGIN_FAIL:
+    case LOGOUT:
       localStorage.removeItem('token');
       return {
         ...state,
